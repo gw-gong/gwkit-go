dd93b2dbd8875a7f569b7e7857967cde7e419179
Guowei Gong <guowei.gong@shopee.com>
2025-06-21 21:35:12 +0800

    impl carrying context info

--- utils/common/context.go
+++ utils/common/context.go
@@ -1,2 +1,22 @@
 package common
 
+import "context"
+
+type ContextKeyRequestID struct{}
+type ContextKeyTraceID struct{}
+
+func SetRequestIDToCtx(ctx context.Context, requestID string) context.Context {
+	return context.WithValue(ctx, ContextKeyRequestID{}, requestID)
+}
+
+func GetRequestIDFromCtx(ctx context.Context) string {
+	return ctx.Value(ContextKeyRequestID{}).(string)
+}
+
+func SetTraceIDToCtx(ctx context.Context, traceID string) context.Context {
+	return context.WithValue(ctx, ContextKeyTraceID{}, traceID)
+}
+
+func GetTraceIDFromCtx(ctx context.Context) string {
+	return ctx.Value(ContextKeyTraceID{}).(string)
+}

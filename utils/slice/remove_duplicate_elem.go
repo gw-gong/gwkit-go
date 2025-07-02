5bcedc2183182bbb3db3a22b7df626927592d3d0
Guowei Gong <guowei.gong@shopee.com>
2025-07-02 14:55:17 +0800

    add tiny tool slice.RemoveDuplicateElem()

---
+++ utils/slice/remove_duplicate_elem.go
@@ -0,0 +1,13 @@
+package slice
+
+func RemoveDuplicateElem[T comparable](slice []T) []T {
+	elemSet := make(map[T]struct{})
+	result := make([]T, 0)
+	for _, elem := range slice {
+		if _, ok := elemSet[elem]; !ok {
+			elemSet[elem] = struct{}{}
+			result = append(result, elem)
+		}
+	}
+	return result
+}

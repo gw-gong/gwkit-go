af739a5435597565734c4c29ec4be4c427f3fc40
Guowei Gong <guowei.gong@shopee.com>
2025-08-11 13:28:16 +0800

    improve with retry

--- utils/common/retry.go
+++ utils/common/retry.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"context"
+	"fmt"
 	"time"
 )
 
@@ -10,7 +11,11 @@ const (
 	MaxBackoff      = 10 * time.Second
 )
 
-func WithRetry(ctx context.Context, f func() (shouldRetry bool, err error), maxTries int, interval time.Duration) (allTries int, allErrors []error, lastError error) {
+// WithRetry is a function that retries a function until it returns false or the context is canceled.
+// maxTries is the maximum number of tries, interval is the interval between tries.
+// allTries is the number of tries, allErrs is the "returned error" of all tries, lastErr is the error of the last try.
+func WithRetry(ctx context.Context, f func() (shouldRetry bool, err error), maxTries int, interval time.Duration) (
+	allTries int, allErrs []error, lastErr error) {
 	if maxTries <= 0 {
 		maxTries = 1
 	}
@@ -18,24 +23,35 @@ func WithRetry(ctx context.Context, f func() (shouldRetry bool, err error), maxT
 		interval = DefaultInterval
 	}
 
-	allErrors = make([]error, 0, maxTries)
-	for i := 0; i < maxTries; i++ {
+	allErrs = make([]error, 0, maxTries)
+	for i := 0; i < maxTries; {
 		shouldRetry, err := f()
-		allErrors = append(allErrors, err)
+		lastErr = err
+		if err != nil {
+			allErrs = append(allErrs, fmt.Errorf("try %d: %w", i+1, lastErr))
+		}
 		if !shouldRetry {
-			return i + 1, allErrors, err
+			return i + 1, allErrs, lastErr
+		}
+
+		// The last loop does not need to wait, just return
+		i++
+		if i >= maxTries {
+			break
 		}
 
 		select {
 		case <-ctx.Done():
-			return i + 1, allErrors, ctx.Err()
+			lastErr = fmt.Errorf("try %d: context canceled: %w", i+1, ctx.Err())
+			allErrs = append(allErrs, lastErr)
+			return i + 1, allErrs, lastErr
 		default:
-			backoff := time.Duration(1<<uint(i)) * interval
+			backoff := time.Duration(1<<uint(i-1)) * interval
 			if backoff > MaxBackoff {
 				backoff = MaxBackoff
 			}
 			time.Sleep(backoff)
 		}
 	}
-	return maxTries, allErrors, allErrors[len(allErrors)-1] // Logically, this will not be out of bounds
+	return maxTries, allErrs, lastErr
 }

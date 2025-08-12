c7559e43ef5c2d0dcedbf9fd086ca5c1da85e8f9
Guowei Gong <guowei.gong@shopee.com>
2025-08-12 11:28:48 +0800

    impl ants logger interface

---
+++ concurrency/ants_tool/logger.go
@@ -0,0 +1,26 @@
+package ants_tool
+
+import (
+	"context"
+
+	"github.com/gw-gong/gwkit-go/log"
+)
+
+/*
+
+Implement the Logger interface of github.com/panjf2000/ants/v2
+
+type Logger interface {
+	// Printf must have the same semantics as log.Printf.
+	Printf(format string, args ...any)
+}
+
+*/
+
+type SugarLogger struct {
+	ctx context.Context
+}
+
+func (l *SugarLogger) Printf(format string, args ...interface{}) {
+	log.Infofc(l.ctx, format, args...)
+}
--- log/logger_func.go
+++ log/logger_func.go
@@ -6,6 +6,8 @@ import (
 	"go.uber.org/zap"
 )
 
+// ================================ Logger Functions ================================
+
 func Debug(msg string, fields ...field) {
 	zap.L().Debug(msg, fields...)
 }
@@ -41,6 +43,46 @@ func Errorc(ctx context.Context, msg string, fields ...field) {
 	loggerFromCtx := getLoggerFromCtx(ctx)
 	loggerFromCtx.Error(msg, fields...)
 }
+
+// ================================ Sugar Functions ================================
+
+func Debugf(format string, args ...interface{}) {
+	zap.L().Sugar().Debugf(format, args...)
+}
+
+func Infof(format string, args ...interface{}) {
+	zap.L().Sugar().Infof(format, args...)
+}
+
+func Warnf(format string, args ...interface{}) {
+	zap.L().Sugar().Warnf(format, args...)
+}
+
+func Errorf(format string, args ...interface{}) {
+	zap.L().Sugar().Errorf(format, args...)
+}
+
+func Debugfc(ctx context.Context, format string, args ...interface{}) {
+	loggerFromCtx := getLoggerFromCtx(ctx)
+	loggerFromCtx.Sugar().Debugf(format, args...)
+}
+
+func Infofc(ctx context.Context, format string, args ...interface{}) {
+	loggerFromCtx := getLoggerFromCtx(ctx)
+	loggerFromCtx.Sugar().Infof(format, args...)
+}
+
+func Warnfc(ctx context.Context, format string, args ...interface{}) {
+	loggerFromCtx := getLoggerFromCtx(ctx)
+	loggerFromCtx.Sugar().Warnf(format, args...)
+}
+
+func Errorfc(ctx context.Context, format string, args ...interface{}) {
+	loggerFromCtx := getLoggerFromCtx(ctx)
+	loggerFromCtx.Sugar().Errorf(format, args...)
+}
+
+// ================================ With Fields Functions ================================
 
 func WithFields(ctx context.Context, fields ...field) context.Context {
 	loggerFromCtx := getLoggerFromCtx(ctx)

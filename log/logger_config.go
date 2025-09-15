e193822b26b25a855620a5dc85b093335615859a
Guowei Gong <guowei.gong@shopee.com>
2025-09-15 16:39:58 +0800

    add viper tag (log config)

--- log/logger_config.go
+++ log/logger_config.go
@@ -8,31 +8,31 @@ const (
 )
 
 type LoggerConfig struct {
-	Level           string                `yaml:"level" json:"level"`                         // Log level
-	OutputToFile    OutputToFileConfig    `yaml:"output_to_file" json:"output_to_file"`       // Output to file configuration
-	OutputToConsole OutputToConsoleConfig `yaml:"output_to_console" json:"output_to_console"` // Output to console configuration
-	AddCaller       bool                  `yaml:"add_caller" json:"add_caller"`               // Whether to add caller information
-	StackTrace      StackTraceConfig      `yaml:"stack_trace" json:"stack_trace"`             // Stack trace configuration
+	Level           string                `yaml:"level" json:"level" mapstructure:"level"`                                     // Log level
+	OutputToFile    OutputToFileConfig    `yaml:"output_to_file" json:"output_to_file" mapstructure:"output_to_file"`          // Output to file configuration
+	OutputToConsole OutputToConsoleConfig `yaml:"output_to_console" json:"output_to_console" mapstructure:"output_to_console"` // Output to console configuration
+	AddCaller       bool                  `yaml:"add_caller" json:"add_caller" mapstructure:"add_caller"`                      // Whether to add caller information
+	StackTrace      StackTraceConfig      `yaml:"stack_trace" json:"stack_trace" mapstructure:"stack_trace"`                   // Stack trace configuration
 }
 
 type OutputToFileConfig struct {
-	Enable     bool   `yaml:"enable" json:"enable"`           // Whether to enable output
-	FilePath   string `yaml:"path" json:"path"`               // File path
-	WithBuffer bool   `yaml:"with_buffer" json:"with_buffer"` // Whether to use buffer, will not be immediately flushed to the file
-	MaxSize    int    `yaml:"max_size" json:"max_size"`       // Maximum file size (MB)
-	MaxBackups int    `yaml:"max_backups" json:"max_backups"` // Maximum number of backup files
-	MaxAge     int    `yaml:"max_age" json:"max_age"`         // Maximum retention days
-	Compress   bool   `yaml:"compress" json:"compress"`       // Whether to compress after rotation
+	Enable     bool   `yaml:"enable" json:"enable" mapstructure:"enable"`                // Whether to enable output
+	FilePath   string `yaml:"path" json:"path" mapstructure:"path"`                      // File path
+	WithBuffer bool   `yaml:"with_buffer" json:"with_buffer" mapstructure:"with_buffer"` // Whether to use buffer, will not be immediately flushed to the file
+	MaxSize    int    `yaml:"max_size" json:"max_size" mapstructure:"max_size"`          // Maximum file size (MB)
+	MaxBackups int    `yaml:"max_backups" json:"max_backups" mapstructure:"max_backups"` // Maximum number of backup files
+	MaxAge     int    `yaml:"max_age" json:"max_age" mapstructure:"max_age"`             // Maximum retention days
+	Compress   bool   `yaml:"compress" json:"compress" mapstructure:"compress"`          // Whether to compress after rotation
 }
 
 type OutputToConsoleConfig struct {
-	Enable   bool   `yaml:"enable" json:"enable"`     // Whether to enable output
-	Encoding string `yaml:"encoding" json:"encoding"` // Encoding
+	Enable   bool   `yaml:"enable" json:"enable" mapstructure:"enable"`       // Whether to enable output
+	Encoding string `yaml:"encoding" json:"encoding" mapstructure:"encoding"` // Encoding
 }
 
 type StackTraceConfig struct {
-	Enable     bool   `yaml:"enable" json:"enable"` // Whether to enable stack trace
-	TraceLevel string `yaml:"level" json:"level"`   // Stack trace level
+	Enable     bool   `yaml:"enable" json:"enable" mapstructure:"enable"` // Whether to enable stack trace
+	TraceLevel string `yaml:"level" json:"level" mapstructure:"level"`    // Stack trace level
 }
 
 func IsSupportedEncodingType(encoding string) bool {

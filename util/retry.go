8fa42205a41a9758a126ffb5c42100309335a445
Guowei Gong <guowei.gong@shopee.com>
2025-12-18 22:12:12 +0800

    simplify withretry

--- util/retry.go
+++ util/retry.go
@@ -7,28 +7,26 @@ import (
 )
 
 const (
-	DefaultInterval = 10 * time.Millisecond
-	MaxBackoff      = 10 * time.Second
+	DefaultMaxTries      = 3
+	DefaultRetryInterval = 10 * time.Millisecond
+	MaxBackoff           = 10 * time.Second
 )
 
 // WithRetry is a function that retries a function until it returns false or the context is canceled.
 // maxTries is the maximum number of tries, interval is the interval between tries.
 // allTries is the number of tries, allErrs is the "returned error" of all tries, lastErr is the error of the last try.
-func WithRetry(ctx context.Context, operationName string, maxTries int, interval time.Duration, operationFunc func() (shouldRetry bool, err error)) (allTries int, allErrs []error, lastErr error) {
+func WithRetry(ctx context.Context, maxTries int, interval time.Duration, operationFunc func() (shouldRetry bool, err error)) (allTries int, allErrs []error, lastErr error) {
 	if ctx == nil {
 		return 0, []error{fmt.Errorf("context cannot be nil")}, fmt.Errorf("context cannot be nil")
 	}
 	if operationFunc == nil {
 		return 0, []error{fmt.Errorf("operationFunc cannot be nil")}, fmt.Errorf("operationFunc cannot be nil")
 	}
-	if operationName == "" {
-		operationName = "unknown operation"
-	}
 	if maxTries <= 0 {
-		maxTries = 1
+		maxTries = DefaultMaxTries
 	}
 	if interval <= 0 {
-		interval = DefaultInterval
+		interval = DefaultRetryInterval
 	}
 
 	allErrs = make([]error, 0, maxTries)
@@ -36,7 +34,7 @@ func WithRetry(ctx context.Context, operationName string, maxTries int, interval
 		shouldRetry, err := operationFunc()
 		lastErr = err
 		if err != nil {
-			allErrs = append(allErrs, fmt.Errorf("%s attempt %d/%d: %w", operationName, i, maxTries, lastErr))
+			allErrs = append(allErrs, fmt.Errorf("try %d/%d failed: %w", i, maxTries, lastErr))
 		}
 		if !shouldRetry {
 			return i, allErrs, lastErr
@@ -49,7 +47,7 @@ func WithRetry(ctx context.Context, operationName string, maxTries int, interval
 
 		select {
 		case <-ctx.Done():
-			lastErr = fmt.Errorf("%s canceled after %d/%d attempts: %w", operationName, i, maxTries, ctx.Err())
+			lastErr = fmt.Errorf("canceled after %d/%d try: %w", i, maxTries, ctx.Err())
 			allErrs = append(allErrs, lastErr)
 			return i, allErrs, lastErr
 		default:

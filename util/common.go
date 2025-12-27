6fa2119775c1a0018a43c0973ea0593fd990f482
Guowei Gong <guowei.gong@shopee.com>
2025-12-27 17:55:31 +0800

    replace exit with panic

--- util/common.go
+++ util/common.go
@@ -2,14 +2,14 @@ package util
 
 import (
 	"context"
-	"os"
+	"runtime/debug"
 
 	"github.com/gw-gong/gwkit-go/log"
 )
 
 func ExitOnErr(ctx context.Context, err error) {
 	if err != nil {
-		log.Errorc(ctx, "ExitOnErr", log.Err(err))
-		os.Exit(1)
+		log.Errorc(ctx, "ExitOnErr", log.Str("stack", string(debug.Stack())), log.Err(err))
+		panic("ExitOnErr: " + err.Error())
 	}
 }

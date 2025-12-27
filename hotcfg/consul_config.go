83eb27ea08828a7eae88b7dbf8ddd9c39fe8ae8e
Guowei Gong <guowei.gong@shopee.com>
2025-12-27 22:22:42 +0800

    update consul cfg labels

--- hotcfg/consul_config.go
+++ hotcfg/consul_config.go
@@ -7,8 +7,8 @@ type ConsulConfig interface {
 }
 
 type ConsulConfigOption struct {
-	ConsulAddr string `json:"consulAddr" yaml:"consulAddr" mapstructure:"consulAddr"`
-	ConsulKey  string `json:"consulKey" yaml:"consulKey" mapstructure:"consulKey"`
-	ConfigType string `json:"configType" yaml:"configType" mapstructure:"configType"`
-	ReloadTime int    `json:"reloadTime" yaml:"reloadTime" mapstructure:"reloadTime"` // second
+	ConsulAddr string `json:"consul_addr" yaml:"consul_addr" mapstructure:"consul_addr"`
+	ConsulKey  string `json:"consul_key" yaml:"consul_key" mapstructure:"consul_key"`
+	ConfigType string `json:"config_type" yaml:"config_type" mapstructure:"config_type"`
+	ReloadTime int    `json:"reload_time" yaml:"reload_time" mapstructure:"reload_time"` // second
 }
